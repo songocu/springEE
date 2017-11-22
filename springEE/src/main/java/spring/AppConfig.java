618b3e51d21e967be772046132d2a12912cd6519
tud <tud@tudWin10>
2017-11-22 17:47:41 +0200

    Add a comment to know how it was at beginning, did not work for strange reasons, and needed to be separated in MyViewResolver.java

--- springEE/src/main/java/spring/AppConfig.java
+++ springEE/src/main/java/spring/AppConfig.java
@@ -8,5 +8,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages={"beans","controller","database","database.dao","services","spring"})
 public class AppConfig {
-	
+
+	/*
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver();
+	}
+	*/
 }
